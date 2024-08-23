cfaf33e302ff168914e3157c575b014252068433
devnshankar <subhashankarbehera7@gmail.com>
2024-08-23 15:40:33 +0530

    Added the images

--- src/components/HomeComponents/HomeComp1.js
+++ src/components/HomeComponents/HomeComp1.js
@@ -3,9 +3,9 @@ import React from "react";
 import Image from "next/image";
 import styles from "../../styles/Home/HomeComp1.module.css";
 import { homeComp1Data } from "./HomeConstantDatas";
-import ButtonComp from "../ButtonComp";
 import { Grid } from "@mui/material";
 import { montserrat } from "@/assets/fonts/fonts";
+import HomeButtonComp from "../HomeButtonComp";
 
 export default function HomeComp1() {
   return (
@@ -24,7 +24,7 @@ export default function HomeComp1() {
       </div>
       <div className={styles.hero1heroText}>{homeComp1Data.heroText}</div>
       <div className={styles.hero1heroSubText}>{homeComp1Data.heroSubText}</div>
-      <ButtonComp title="Get Started" className={styles.hero1buttonComp} />
+      <HomeButtonComp title="Get Started" className={styles.hero1buttonComp} />
       <div className={styles.hero1scrollContainer}>
         <Image
           src={homeComp1Data.scrollButton}
--- src/components/HomeComponents/HomeComp4.js
+++ src/components/HomeComponents/HomeComp4.js
@@ -2,7 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../../styles/Home/HomeComp4.module.css";
-import { blogImage1, blogImage2, blogImage3, blogImage4, homeComp4Data } from "./HomeConstantDatas";
+import { homeComp4Data } from "./HomeConstantDatas";
 import { montserrat } from "@/assets/fonts/fonts";
 import { Grid } from "@mui/material";
 
@@ -11,26 +11,21 @@ export default function HomeComp4() {
     <Grid className={`${styles.hero4} ${montserrat.variable}`}>
       <div className={styles.hero4Title}>Blog Posts</div>
       <div className={styles.hero4Grid}>
-        <div className={styles.hero4BlogItem}>
-          <Image src={homeComp4Data.blogposts[0]} alt="Blog 1" className={styles.hero4BlogImage} />
-          <div className={styles.hero4BlogItemTitle}>Over Ride The Digital Design</div>
-          <div className={styles.hero4BlogItemDate}>August 1, 2024</div>
-        </div>
-        <div className={styles.hero4BlogItem}>
-          <Image src={blogImage2} alt="Blog 2" className={styles.hero4BlogImage} />
-          <div className={styles.hero4BlogItemTitle}>Free consultation with experts</div>
-          <div className={styles.hero4BlogItemDate}>August 20, 2024</div>
-        </div>
-        <div className={styles.hero4BlogItem}>
-          <Image src={blogImage3} alt="Blog 3" className={styles.hero4BlogImage} />
-          <div className={styles.hero4BlogItemTitle}>Quality Design projects completed</div>
-          <div className={styles.hero4BlogItemDate}>August 22, 2024</div>
-        </div>
-        <div className={styles.hero4BlogItem}>
-          <Image src={blogImage4} alt="Blog 4" className={styles.hero4BlogImage} />
-          <div className={styles.hero4BlogItemTitle}>Task completion planning with timelines</div>
-          <div className={styles.hero4BlogItemDate}>August 25, 2024</div>
-        </div>
+        {homeComp4Data.blogposts.map((post, index) => (
+          <div key={index} className={styles.hero4BlogItem}>
+            <Image
+              src={post.img}
+              alt={post.alt}
+              width={400}
+              height={300}
+              className={styles.hero4BlogImage}
+            />
+            <div className={styles.hero4BlogItemTitle}>{post.alt}</div>
+            <div className={styles.hero4BlogItemDate}>
+              August {index + 1}, 2024
+            </div>
+          </div>
+        ))}
       </div>
     </Grid>
   );
