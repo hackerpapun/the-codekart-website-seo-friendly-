8bc08c3c18f9b272830dfbbea00ea14a339501fd
devnshankar <subhashankarbehera7@gmail.com>
2024-08-30 10:36:27 +0530

    Home page responsive partially done

--- src/components/HomeComponents/HomeComp7.js
+++ src/components/HomeComponents/HomeComp7.js
@@ -1,17 +1,30 @@
 "use client";
 import React from "react";
 import styles from "../../styles/Home/HomeComp7.module.css";
-import Button from "@mui/material/Button";
+import { Divider, Grid } from "@mui/material";
+import ButtonCustom from "../ButtonCustom";
+import { notosans } from "@/assets/fonts/fonts";
 
 export default function HomeComp7() {
   return (
-    <div className={styles.hero7}>
-      <div className={styles.hero7Title}>
-        Request a proposal or consult, Feel free to contact us Please.
-      </div>
-      <Button variant="contained" className={styles.hero7Button}>
-        Contact Us
-      </Button>
-    </div>
+    <Grid container className={`${styles.whatwedoComp4Div} ${notosans.variable}`}>
+      <Grid item xs={12} md={8}>
+        <div className={styles.whatwedoComp4Text}>
+          Request a proposal or consult,
+          <br />
+          Feel free to contact us Please.
+        </div>
+      </Grid>
+      <Grid item xs={12} md={3} className={styles.whatwedoComp4ButtonContainer}>
+        <ButtonCustom
+          title="Contact Us"
+          icon={true}
+          buttonStyles={styles.whatwedoComp4Button}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Divider sx={{ width: '90%', height: '1px', margin: 'auto', backgroundColor: 'darkgray', marginTop: '40px'}} />
+      </Grid>
+    </Grid>
   );
 }
