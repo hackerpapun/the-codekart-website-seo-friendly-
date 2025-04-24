8be3fb3c8953b13a0a26cc08e450c536c3c27d54
@hacker <129648421+hackerpapun@users.noreply.github.com>
2025-04-24 15:41:54 +0530

    changes...

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "export", 
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
   experimental: {
-    serverActions: true,
+    serverActions: true, 
   },
 };
 
